29e37f4e413ca59bd98866bfbb09155f7408c1e1
Zara Ray <zararaymond05@gmail.com>
2023-05-22 22:12:52 +0000

    file updates

---
+++ script.sh
@@ -0,0 +1,61 @@
+# update the ubuntu server
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# install unzip
+
+sudo apt-get install unzip
+
+# install terraform
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt-get update && sudo apt-get install terraform
+
+
+# install jenkins
+
+sudo apt-get update
+
+sudo apt install openjdk-11-jre -y
+
+java -version
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins -y
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+sudo ufw allow OpenSSH 
+
+sudo ufw enable
+
+sudo ufw allow 8080
+
+sudo ufw status
+
+
